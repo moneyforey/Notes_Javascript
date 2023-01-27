703f89eea63e4932f03c57cb4f7c3b44897cbff1
moneyforey <moneyforey786@gmail.com>
2023-01-27 12:32:38 +0530

    init commit

---
+++ datatype.js
@@ -0,0 +1,37 @@
+//primitive data type - predefined data type;
+
+//Number
+let number = 23;
+
+//string
+
+let string = 'alpha';
+
+//symbol
+
+let symbol = Symbol('hello') //immutable data type;
+
+//bigInt :- number of large size;
+
+// boolean
+
+let boolean = true;
+
+//undefined;
+
+let x;
+
+//null 
+
+let y = null;
+
+
+//non-primitive data type:- derived from premivitive data type
+
+//Array;- index-value pair
+let arr = [];
+
+//object :- key-value pair
+let obj = {};
+
+
