08cd976800566a655fe8419dcca216eb4113b3d7
moneyforey <moneyforey786@gmail.com>
2023-01-27 12:43:11 +0530

    short circut

---
+++ shortcircut.js
@@ -0,0 +1,22 @@
+//short circuting in javascript;
+ 
+false && true //false;
+
+true && true //true;
+
+false || true //true;
+
+true || true //true;
+
+false || false //false;
+
+5 && 3 //3;
+
+5 || 3 //5;
+
+0 || 5 // 5
+
+0 && 5 //0;
+
+
+
