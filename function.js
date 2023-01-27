d2c3ca7efdf493e77852ee74c0ab3b3f47b8dd83
moneyforey <moneyforey786@gmail.com>
2023-01-27 17:22:44 +0530

    functions

---
+++ function.js
@@ -0,0 +1,17 @@
+//simple function
+
+function Funx(){
+    let x = 5;
+    let y = 4;
+    let sum = x+y;
+    console.log(sum);
+}
+
+//arrow function;
+
+const ArrowFunc=()=>{
+    let x = 5;
+    let y = 4;
+    let sum = x+y;
+    console.log(sum);
+}
